3654b35d29ac976870de0636c0490d9191824713
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-08-09 19:39:13 +0530

    Add last played song.

--- scrobble.js
+++ scrobble.js
@@ -1,10 +1,10 @@
 function welcomeFn() {
   var userName = document.getElementById("userName").value;
   if (userName == "") {
-    UIkit.notification({message: '<span uk-icon=\'icon: warning\'></span> Error! Enter a Last.fm user name.', status: 'danger'})
+    UIkit.notification({message: '<span uk-icon=\'icon: warning\'></span> Error! Enter a Last.fm user name.', status: 'danger'});
   }
   else {
-    UIkit.notification({message: '<span uk-icon=\'icon: check\'></span> Visualising your data now. Scroll down if on a mobile device.', status: 'primary'})
+    UIkit.notification({message: '<span uk-icon=\'icon: check\'></span> Visualising your data now. Scroll down if on a mobile device.', status: 'primary'});
   }
   $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getinfo&api_key=6e616452b7c762a15256272ddb774c56&user=" + userName + "&format=json", function(json) {
     var html = '';
@@ -32,12 +32,18 @@ function welcomeFn() {
     var diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
     console.log(imgSrc);
     var songsPerDay = (totalScrobbles/diffDays).toFixed();
+    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&nowplaying=true&api_key=6e616452b7c762a15256272ddb774c56&user=" + userName + "&format=json", function(json) {
+      var nowplaying = json.recenttracks.track[0]['name'];
+      var lastplayedImg = json.recenttracks.track[0]['image'][2]['#text'];
+      var lastartistname = json.recenttracks.track[0]['artist']['#text'];
+      var lastalbumname = json.recenttracks.track[0]['album']['#text'];
+      console.log(lastplayedImg);
+      document.getElementById("recentTracks").innerHTML = "Your last played song is : " + nowplaying + " by : " + lastartistname + " from the Album : " + lastalbumname;
+      document.getElementById('lastplayed').src = lastplayedImg;
+    });
 
     document.getElementById("welcome").innerHTML = "Hi" + " " + lastfmUser;
     document.getElementById("totalScrobbles").innerHTML = "You have scrobbled a total of" + " " + totalScrobbles + " songs since joining Last.fm on " + convdataTime + " It means " + diffDays + " days have elapsed since then! Oh, it also means that you have scrolled " + songsPerDay + " songs per day! Keep it up.";
     document.getElementById('image').src = imgSrc;
   });
-}
-function getData() {
-
 }
