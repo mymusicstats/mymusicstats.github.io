5a3db71782a04a46de4655a47ab54053e1a1142c
Shashank Srivastava <shashank.ssriv@gmail.com>
2018-08-10 19:18:44 +0530

    Add top 10 songs/artists/albums with animation effects.

--- tests/submit.js
+++ tests/submit.js
@@ -1,7 +1,7 @@
 describe('My Music Stats Submit button test', function() {
   it('should take the username & fetch details', function() {
     browser.driver.get('http://localhost/~admin/mymusicstats/');
-    expect(browser.driver.getTitle()).toEqual('My Music Stats - Visualise your Last.fm Data, analyse your music habits.');
+    expect(browser.driver.getTitle()).toEqual('My Music Stats - Visualise your Last.fm data, analyse your music habits.');
     browser.driver.findElement(by.id('userName')).sendKeys('shashnk-ssriva');
     browser.driver.findElement(by.id('submitBtn')).click();
   });
